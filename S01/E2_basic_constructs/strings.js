bcd71fdd0393ae34a7323ecf4066d367eeac7671
Magdalena <tyszkiewicz.konopqa@gmail.com>
2024-03-25 10:19:16 +0100

    Organize in folders and add string operations

---
+++ S01/E2_basic_constructs/strings.js
@@ -0,0 +1,16 @@
+// const userName = 'Magdalena'
+// const userSurname = 'Konopka'
+
+// let concatenatedString = userName + userSurname;
+// let separatedConcatenatedString = userName + ' ' + userSurname;
+
+// console.log('Hello', userName, userSurname)
+// console.log(`Hello ${userName} ${userSurname}`)
+
+let testType = 'integration tests'
+let testPassed = 'passed'
+let testFailed = 'failed'
+
+console.log(testType, testPassed, testFailed)
+console.log(`${testType}: ${testPassed}`)
+console.log(`${testType}: ${testFailed}`)
